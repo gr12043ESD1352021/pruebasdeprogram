19152695d610e40afe0e7c6d858e9ae75ea8237c
chaic <chaic@DESKTOP-R9RR6C5>
2021-09-07 21:29:51 -0600

    prueba memoria dinamica

--- main.c
+++ main.c
@@ -1,78 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <io.h>
 
-struct Pelicua {
+struct Pelicula {
     /*arreglando matriz*/
-    int **Matriz;
-    int **MatrizInversa;
-    int **MultiploDeMatrices;
+    int Matriz[6][59];
+    //    int MatrizInversa[59][6];
+    //    int MultiploDeMatrices[59][59];
 } Peliculas;
 
 int main(int argc, char** argv) {
-
-    Peliculas.Matriz = (int**) malloc(6 * sizeof (int));
-    Peliculas.MatrizInversa = (int**) malloc(58 * sizeof (int*));
-    Peliculas.MultiploDeMatrices = (int**) malloc(58 * sizeof (int*));
-
+    Integrantes();
     // Creamos un obejto para poder abrir archivo de texto
     FILE *Datos = fopen("peliculasFavoritasESD135_2021.csv", "r");
+
     //Validamos ejecucion de lectura de archivo.
     if (Datos == NULL) {
         perror("Error al abrir archivo.");
         return 1;
     }
+    LlenandoMatriz(*Datos);
+    LlenandoInversa();
+    printf("\n\n");
+    fclose(Datos);
+    return 0;
+}
 
-
-    for (int largo = 0; largo < 59; largo++) {
-        for (int ancho = 0; ancho < 6; ancho++) {
-            Peliculas.MatrizInversa[largo][ancho] = 0;
-            Peliculas.Matriz[ancho][largo] = 0;
-        }
-    }
-    for (int largo = 0; largo < 59; largo++) {
-        for (int ancho = 0; ancho < 59; ancho++) {
-            Peliculas.MultiploDeMatrices[largo][ancho] = 0;
-        }
-    }
+void LlenandoMatriz(FILE *Datos) {
 
     int coma = 0, largo = 0, ancho = 0, aux;
     char caracter[60];
     char *token;
     while (fgets(caracter, sizeof (caracter), Datos)) {
-
         token = strtok(caracter, ",");
-
         if (coma > 1) {
             while (token != NULL) {
-
                 if (ancho == 0) {
                     token = strtok(NULL, ",");
                 } else if (ancho > 0) {
                     aux = strtol(token, NULL, 10);
                     if (aux != 0) {
-                        Peliculas.Matriz[ancho-1] = (int*) malloc((largo) * sizeof (int));
-                        Peliculas.Matriz[ancho - 1][largo] = 0;
+                        Peliculas.Matriz[ancho - 1][largo] = aux;
                     } else {
-                        Peliculas.Matriz[ancho-1] = (int*) malloc((largo) * sizeof (int));
                         Peliculas.Matriz[ancho - 1][largo] = 0;
                     }
                     token = strtok(NULL, ",");
                 }
                 ancho++;
             }
-
         } else if (coma <= 1) {
             token = strtok(NULL, ",");
         }
-
         coma++;
-
         if (coma > 1 && ancho >= 6) {
             largo++;
             ancho = 0;
-
         }
     }
     for (int a = 0; a < 59; a++) {
@@ -82,22 +64,28 @@ int main(int argc, char** argv) {
         printf("\n");
     }
     printf("\n\n");
-    LlenandoInversa();
-    printf("\n\n");
-    MultiplicandoMatriz();
-    free(Peliculas.Matriz);
-    free(Peliculas.MatrizInversa);
-    free(Peliculas.MultiploDeMatrices);
-    fclose(Datos);
-    return 0;
 }
 
 void LlenandoInversa() {
+    int **MatrizInversa;
+    MatrizInversa = (int**) malloc(58 * sizeof (int*));
+    if (MatrizInversa == NULL) {
+        printf("Error al reservar meroria.");
+        return 1;
+    }
+    for (int largo = 0; largo < 59; largo++) {
 
+        for (int ancho = 0; ancho < 6; ancho++) {
+            MatrizInversa[largo] = (int*) malloc(ancho * sizeof (int));
+            if (MatrizInversa[largo] == NULL) {
+                printf("Error al reservar meroria.");
+                return 1;
+            }
+        }
+    }
     for (int a = 0; a < 59; a++) {
         for (int b = 0; b < 6; b++) {
-            Peliculas.MatrizInversa[a] = (int*) malloc(b * sizeof (int));
-            Peliculas.MatrizInversa[a][b] = Peliculas.Matriz[b][a];
+            MatrizInversa[a][b] = Peliculas.Matriz[b][a];
         }
     }
     for (int b = 0; b < 6; b++) {
@@ -107,25 +95,47 @@ void LlenandoInversa() {
         }
         printf("\n");
     }
+    MultiplicandoMatriz(**MatrizInversa);
 }
 
-void MultiplicandoMatriz() {
+void MultiplicandoMatriz(int **MatrizInversa) {
+    int **MultiploDeMatrices;
+    MultiploDeMatrices = (int**) malloc(58 * sizeof (int*));
+    if (MultiploDeMatrices == NULL) {
+        printf("Error al reservar meroria.");
+        return 1;
+    }
+    for (int largo = 0; largo < 59; largo++) {
 
-    for (int largoMult = 0; largoMult < 59; largoMult++) {
-        for (int largo = 0; largo < 59; largo++) {
-            for (int ancho = 0; ancho < 6; ancho++) {
-                Peliculas.MultiploDeMatrices[largoMult] = (int*) malloc(largo * sizeof (int));
-                Peliculas.MultiploDeMatrices[largoMult][largo] += Peliculas.Matriz[ancho][largoMult] * Peliculas.MatrizInversa[largo][ancho];
+        for (int ancho = 0; ancho < 59; ancho++) {
+            MultiploDeMatrices[largo] = (int*) malloc(ancho * sizeof (int));
+            if (MultiploDeMatrices[largo] == NULL) {
+                printf("Error al reservar meroria.");
+                return 1;
             }
         }
     }
-
-
+    for (int largoMult = 0; largoMult < 59; largoMult++) {
+        for (int largo = 0; largo < 59; largo++) {
+            for (int ancho = 0; ancho < 6; ancho++) {
+                MultiploDeMatrices[largoMult][largo] += Peliculas.Matriz[ancho][largoMult] * MatrizInversa[largo][ancho];
+            }
+        }
+    }
     for (int b = 0; b < 59; b++) {
         for (int a = 0; a < 59; a++) {
-            printf("%i ", Peliculas.MultiploDeMatrices[b][a]);
+            if (a == b) {
+                printf("n  ");
+            } else {
+                printf("%i  ", MultiploDeMatrices[b][a]);
+            }
         }
         printf("\n");
     }
 }
 
+void Integrantes() {
+    const char *Carnet1 = "GR12043";
+    const char *Carnet2 = "PC19060";
+    printf("Estudiantes: %s       %s\n", Carnet1, Carnet2);
+}
